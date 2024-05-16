e764245c45ef47247844c7e9435d7ce3c7df4a6b
Ridham Patel <105585070+ridhampatel24@users.noreply.github.com>
2024-05-16 13:45:50 +0530

    Update ec2_python_app.sh

--- infra/template/ec2_python_app.sh
+++ infra/template/ec2_python_app.sh
@@ -4,11 +4,14 @@ cd /home/ubuntu
 yes | sudo apt update
 yes | sudo apt install python3 python3-pip
 sudo apt install net-tools -y
+sudo apt-get install coreutils -y
 git clone https://github.com/ridhampatel24/demo_python_app.git
 sleep 20
 cd /home/ubuntu/demo_python_app
 # pip3 install -r requirements.txt
 pip3 install -r requirements.txt --break-system-packages
 echo 'Waiting for 30 seconds before running the app.py'
-setsid python3 -u app.py &
-sleep 30
+sudo nohup python3 -u app.py > log.txt 2>&1 &
+
+#setsid python3 -u app.py &
+sleep 5
