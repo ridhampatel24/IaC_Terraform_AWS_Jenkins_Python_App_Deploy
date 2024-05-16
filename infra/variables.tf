46a2878f6b59b2de1472bee709f54e1887f17b29
Ridham <ridhampatel2002@gmail.com>
2024-05-16 23:02:30 +0530

    Final

--- infra/variables.tf
+++ infra/variables.tf
@@ -48,12 +48,12 @@ variable "ec2_ami_id" {
   description = "DevOps Project 1 AMI Id for EC2 instance"
 }
 
-variable "ec2_user_data_install_apache" {
-  type = string
-  description = "Script for installing the Apache2"
-}
+# variable "ec2_user_data_install_apache" {
+#   type = string
+#   description = "Script for installing the Apache2"
+# }
 
 variable "domain_name" {
-  type = string
+  type        = string
   description = "Name of the domain"
 }
